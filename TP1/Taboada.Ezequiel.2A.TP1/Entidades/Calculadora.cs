06fa74db9e56bb59afbdf9a783b5d6fc315f99ac
ezetabo <ezetabo78@gmail.com>
2022-04-03 21:13:17 -0300

    desarrollo de las clases de entidades

---
+++ TP1/Taboada.Ezequiel.2A.TP1/Entidades/Calculadora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        private static char ValidarOperador(char operador)
+        {
+            if (operador != '+' && operador != '-' && operador != '/' && operador != '*')
+            {
+                operador = '+';
+            }
+            return operador;
+        }
+        public static double Operar(Operando numero1, Operando numero2, char operador)
+        {
+            double resultado = 0;
+            if (numero1 is not null && numero2 is not null)
+            {
+                switch (ValidarOperador(operador))
+                {
+                    case '/':
+                        resultado = numero1 / numero2;
+                        break;
+                    case '*':
+                        resultado = numero1 * numero2;
+                        break;
+                    case '-':
+                        resultado = numero1 - numero2;
+                        break;
+                    default:
+                        resultado = numero1 + numero2;
+                        break;
+                }
+            }
+            return resultado;
+        }
+    }
+}
---
+++ TP1/Taboada.Ezequiel.2A.TP1/Entidades/Operando.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+        private string Numero
+        {
+            set
+            {
+                numero = ValidarOperando(value);
+            }
+        }
+
+        #region Constructores
+        public Operando() : this(0)
+        {
+
+        }
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+        public Operando(string strNumero)
+        {
+            Numero = strNumero;
+        }
+        #endregion
+
+        #region Validaciones
+        private double ValidarOperando(string strNumero)
+        {
+            if (!double.TryParse(strNumero, out double numero))
+            {
+                numero = 0;
+            }
+            return numero;
+        }
+        private bool EsBinario(string binario)
+        {
+            bool retorno = true;
+            foreach (char item in binario)
+            {
+                if (item != '0' && item != '1')
+                {
+                    retorno = false;
+                    break;
+                }
+            }
+            return retorno;
+        }
+        #endregion
+
+        #region Sobrecarga Operadores
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+        public static double operator /(Operando n1, Operando n2)
+        {
+            double resultado = double.MinValue;
+            if (n2.numero != 0)
+            {
+                resultado = n1.numero / n2.numero;
+            }
+            return resultado;
+        }
+        #endregion
+
+        #region Conversores
+        public string DecimalBinario(string strNumero)
+        {
+            string resultado = "Valor Invalido";
+            if (int.TryParse(strNumero, out int numeroEntero) && numeroEntero > -1)
+            {
+                resultado = "";
+                int dato;
+                for (int i = 0; numeroEntero > 0; i++)
+                {
+                    dato = numeroEntero % 2;
+                    numeroEntero = numeroEntero / 2;
+                    resultado = dato.ToString() + resultado;
+                }
+            }
+            return resultado;
+        }
+        public string DecimalBinario(double numero)
+        {
+            return DecimalBinario(numero.ToString());
+        }
+        public string BinarioDecimal(string binario)
+        {
+            string retorno = "Valor Invalido";
+            if (EsBinario(binario))
+            {
+                int resultado = 0;
+                int posicion = binario.Length;
+                foreach (char dato in binario)
+                {
+                    posicion--;
+                    if (dato == '1')
+                    {
+                        resultado += (int)Math.Pow(2, posicion);
+                        retorno = resultado.ToString();
+                    }
+                }
+            }
+            return retorno;
+        }
+        #endregion
+
+    }
+}
