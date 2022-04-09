a4ffadf446112db343b9082fa58faee03dd49fe0
ezetabo <ezetabo78@gmail.com>
2022-04-09 17:15:43 -0300

    arreglo visual en caso de omitir el 0

--- TP1/Taboada.Ezequiel.2A.TP1/MiCalculadora/FormCalculadora.cs
+++ TP1/Taboada.Ezequiel.2A.TP1/MiCalculadora/FormCalculadora.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Entidades;
 
@@ -117,6 +111,72 @@ namespace MiCalculadora
             return esDouble;
         }
 
+        /// <summary>
+        /// revisa el string con formato numerico y agrega el 0 inicial en caso de omision.
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <returns></returns>
+        private string ValidarFormato(string texto)// se puede mejorar - revisar.
+        {
+            string salida = string.Empty;
+            if (texto == string.Empty)
+            {
+                salida = "0";
+            }
+            else
+            {
+                if (texto.Length == 1)
+                {
+                    if (texto == "-")
+                    {
+                        salida = "-0";
+                    }
+                    else
+                    {
+                        if (texto == ".")
+                        {
+                            salida = "0.0";
+                        }
+                        else
+                        {
+                            salida = texto;
+                        }
+                    }
+
+                }
+                else
+                {
+
+                    if (texto[0] == '-' && texto[1] == '.')
+                    {
+                        if (texto.Length == 2)
+                        {
+                            salida = "-0.0";
+                        }
+                        else
+                        {
+                            salida = texto.Substring(0, 1) + '0' + texto.Substring(1);
+                        }
+
+                    }
+                    else
+                    {
+                        if (texto[0] == '.')
+                        {
+                            salida = "0" + texto;
+                        }
+                        else
+                        {
+                            salida = texto;
+                        }
+
+                    }
+                }
+
+            }
+            return salida;
+        }
+
         #endregion|
 
         #region Evento_Load
@@ -181,8 +241,8 @@ namespace MiCalculadora
             {
                 operador = "+";
             }
-            this.lblResultado.Text = Convert.ToString(Operar(this.txtNumero1.Text, this.txtNumero2.Text, operador));
-            this.lstOperaciones.Items.Add(this.txtNumero1.Text + " " + operador + " " + this.txtNumero2.Text + " = " + this.lblResultado.Text + "\n");
+            this.lblResultado.Text = Convert.ToString(Operar(ValidarFormato(this.txtNumero1.Text), ValidarFormato(this.txtNumero2.Text), operador));
+            this.lstOperaciones.Items.Add(ValidarFormato(this.txtNumero1.Text) + " " + operador + " " + ValidarFormato(this.txtNumero2.Text) + " = " + this.lblResultado.Text + "\n");
         }
 
         /// <summary>
@@ -257,5 +317,7 @@ namespace MiCalculadora
         }
 
         #endregion
+
+
     }
 }
